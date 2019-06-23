8f6ff393466bf17a4fe554702b4400af4e618152
zeddan <robin.saaf@gmail.com>
2019-06-23 17:51:35 +0200

    First working

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
+// import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.init()
+    this.state = {
+      videos: []
+    }
+
+
+    window['onYouTubeIframeAPIReady'] = (e) => {
+      this.YT = window['YT']
+      this.player = new window['YT'].Player('player', {
+        videoId: 'M7lc1UVf-VE',
+        height: '390',
+        width: '640',
+        events: {
+          // 'onStateChange': this.onPlayerStateChange.bind(this),
+          // 'onError': this.onPlayerError.bind(this),
+          'onReady': (e) => {}
+        }
+      })
+    }
+  }
+
+  componentDidMount() {
+    axios.get('https://api.discogs.com/labels/869700/releases').then((res) => {
+      const releases = res.data.releases.map((release) => {
+        return release.resource_url;
+      });
+
+      const videos = releases.map((releaseUrl) => {
+        axios.get(releaseUrl).then((res) => {
+          return res.data.videos;
+        });
+      });
+    });
+
+  }
+
+  init() {
+    var tag = document.createElement('script')
+    tag.src = 'https://www.youtube.com/iframe_api'
+    var firstScriptTag = document.getElementsByTagName('script')[0]
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag)
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div id="player"></div>
+        <div id="list"></div>
+      </div>
+    );
+  }
 }
 
 export default App;
