d0663ca0f8e528dd5b7c13bf001e23651083cd97
zeddan <robin.saaf@gmail.com>
2021-01-09 20:55:50 +0100

    Chaos commit with no regrets

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import './App.css';
+import './App.scss';
 
 class App extends Component {
 
@@ -9,7 +9,7 @@ class App extends Component {
     this.init()
     this.state = {
       videos: [],
-      currentVideo: ''
+      currentlyPlayingIndex: -1
     }
 
     window['onYouTubeIframeAPIReady'] = (e) => {
@@ -18,50 +18,70 @@ class App extends Component {
         videoId: '03gkNcYKj3k',
         height: '390',
         width: '640',
+        events: {
+          onStateChange: this.onPlayerStateChange.bind(this),
+        }
       })
     }
   }
 
+  onPlayerStateChange(e) {
+    if (e.data === this.YT.PlayerState.ENDED) {
+      let nextIdx = (this.state.nowPlayingIdx + 1) % this.state.videos.length
+      this.play(this.state.videos[nextIdx].uri)
+    }
+  }
+
   init() {
-    var tag = document.createElement('script')
+    const tag = document.createElement('script')
     tag.src = 'https://www.youtube.com/iframe_api'
-    var firstScriptTag = document.getElementsByTagName('script')[0]
+    const firstScriptTag = document.getElementsByTagName('script')[0]
     firstScriptTag.parentNode.insertBefore(tag, firstScriptTag)
   }
 
   play = (uri) => {
     let id = uri.replace("https://www.youtube.com/watch?v=", "")
+    let nowPlayingIdx = this.state.videos.findIndex((e) => e.uri == uri)
+    this.setState({ nowPlayingIdx })
     this.player.loadVideoById({ videoId: id })
   }
 
   search = async () => {
     this.setState({ videos: [] })
+
+    const token = 'a_discogs_token'
     const id = document.getElementById("search-input").value
-    const res = await axios.get(`https://api.discogs.com/labels/${id}/releases`)
+    const res = await axios.get(`https://api.discogs.com/labels/${id}/releases?${token}`)
     const releases = res.data.releases.map((release) => {
       return release.resource_url;
     });
+
     releases.forEach((releaseUrl) => {
-      axios.get(releaseUrl).then((res) => {
+      axios.get(`${releaseUrl}?token=${token}`).then((res) => {
+        if (!res.data.videos)
+          return
         res.data.videos.forEach((video) => {
-          this.setState({
-            videos: [...this.state.videos, video]
-          });
+          if (!this.videoExists(video.uri)) {
+            this.setState({
+              videos: [...this.state.videos, video]
+            });
+          }
         })
-      });
+      })
     });
   }
+
+  videoExists = (uri) => this.state.videos.find(e => e.uri === uri)
 
   setThumbnailBg = (uri) => {
     let id = uri.replace("https://www.youtube.com/watch?v=", "")
     let imgUrl = `https://img.youtube.com/vi/${id}/hqdefault.jpg`
     return ({
-      backgroundImage: 'url(' + imgUrl + ')'
+      backgroundImage: `url(${imgUrl})`
     })
   }
 
   render() {
-    var divStyle = {  }
     return (
       <div className="App">
         <div id="search-container">
